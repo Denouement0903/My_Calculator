dcb474a36182dd0b1963ade8ddf29c187c952013
Simp4Hitagi <denorauten@gmail.com>
2022-11-21 14:51:58 +0200

    Calculator

---
+++ js/code.js
@@ -0,0 +1,21 @@
+// display
+let output = document.querySelector("#output")
+
+// buttons
+let btn0 = document.querySelector("#btn0");
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6");
+let btn7 = document.querySelector("#btn7");
+let btn8 = document.querySelector("#btn8");
+let btn9 = document.querySelector("#btn9");
+
+
+
+btn0.addEventListener('click', ()=>{
+    
+})
+
