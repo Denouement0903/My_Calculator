082c4bb0abd61ba8e8f299b08e334a8221f92990
Simp4Hitagi <denorauten@gmail.com>
2022-11-25 09:15:40 +0200

    New Calc

--- js/code.js
+++ js/code.js
@@ -1,24 +1,65 @@
-let output = document.querySelector("#output");
-// output operation for buttons
-function operation(anyValue) {
-    // increment button values to output field
-    output.value = output.value + anyValue;
-}
-// equal
-function result() {
-    try {
-        output.value = eval(output.value);
-    }
-    catch(error) {
-        alert("Does not compute!")
-    }
-}
-// slice is an array method
-function del() {
-    output.value = output.value.slice(0,-1);
-}
-// to clear
-function clr() {
-    output.value = "";
+// output
+// collect and store user input
+let output = document.querySelector("#outputBox");
+
+
+// actions for functions
+function operation(anyNumber) {
+    // increment button value into output box
+    output.value = output.value + anyNumber; 
 }
 
+
+
+// final equal result
+function finalResult() {
+    // exception handling for invalid input
+    try{
+        output.value=eval(output.value)
+    }
+    // error is a built in argument
+    catch(error){
+        output.value = ("Error!");
+    }
+}
+
+// to clear box
+function clr(){
+    output.value="";
+}
+
+function del(){
+    // array method~slice(start,end)
+    output.value=output.value.slice(0,-1);
+}
+
+
+// for operators
+
+// let array = [];
+
+// function operator(anyValue) {
+//     if (output.value == ".") {
+//         // output.value = "";
+//         output.value = output.value + anyValue; 
+        
+//     } else {
+//         output.value=eval(output.value)
+//     }
+// }
+
+
+// output.value = output.value + anyValue;
+// array.push('+', ".")
+// if (array.includes("+" || ".")) {
+    // output.value=eval(output.value)
+// } else {
+    // output.value = "";
+
+// }
+
+// if (output.value == "++" || output.value == "--" || output.value == "**" ||
+// output.value == "//" || output.value == "%%" || output.value == ".." || output.value == (anyValue +"+" )) {
+    
+// } else {
+// }
