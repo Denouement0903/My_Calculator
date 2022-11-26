b455a2ba06a411e9e50a789f43f567f9c494517c
Simp4Hitagi <denorauten@gmail.com>
2022-11-26 09:48:57 +0200

    Update

--- js/code.js
+++ js/code.js
@@ -1,30 +1,35 @@
 // output
 // collect and store user input
 let output = document.querySelector("#outputBox");
-// let comma = true;
+let array = [];
 
-// let array = [];
-// output.value = output.value + anyNumber;
-// array.push(output.value)
-// if (array.includes(".")) {
-//     output.value = "";
-//     } else {
-//     output.value=eval(output.value)
-// }
+// adding button values to input field 
+function operation(anyValue) {
+    switch(anyValue) {
+        case ".":
+            if (!array.includes('.') ) {
+                output.value += anyValue;
+                array.push(anyValue);
+                console.log(array);
+            }
+            break;
+        case "+":
+        case "%":
+        case "/":
+        case "-":
+        case "*":
+            if (!output.value.endsWith(anyValue)){
+                output.value += anyValue;
+                array = [];
+                break;
+            } 
+            // (!output.value.endsWith(anyValue) ? (output.value += anyValue, array = []) : output.value = output.value)
+        default:
+            array.push(anyValue);
+            output.value += anyValue;
+            console.log(array)
 
-// actions for functions
-function operation(anyNumber) {
-    //checks if output value is not null
-    if(output.value){
-        //checks if pressed operation is a dot and a dot is already there. If so, will quit the function using return
-        if(output.value.includes('.') && anyNumber=='.'){
-            return;
-        }
     }
-    // increment button value into output box
-    ((output.value === "."
-    ) ? (output.value = "") : output.value += anyNumber)
-    // output.value += anyNumber;
 }
 
 // final equal result
@@ -35,27 +40,42 @@ function finalResult() {
     }
     // error is a built in argument
     catch(error){
-        output.value = ("Error!");
+        output.value = "";
     }
 }
 
 // to clear box
 function clr(){
     output.value="";
-    // comma = true;
+    array = [];
 }
 
 function del(){
     // array method~slice(start,end)
     output.value=output.value.slice(0,-1);
-    // comma = true;
+    array.pop()
 }
 
+//checks if output value is not null
+// if(output.value){
+    //checks if pressed operation is a dot and if a dot is already there. If so, will quit the function using return
+//     if(output.value.includes('.') && anyValue=='.'){
+//         if(output.value.includes('+')){
+//             output.value += anyValue; 
+//         };
+//         return;
+//     }
+// }
+// output.value += anyValue;
 
 
+// output.value += anyValue;
+// increment button value into output box
+// ((output.value === "."
+// ) ? (output.value = "") : output.value += anyValue)
 // if (output.value === ".") {
-//     output.value = "";
-
+    //     output.value = "";
+    
 // } else {
 //     output.value += anyNumber;
 
@@ -70,12 +90,20 @@ function del(){
 
 // function operator(anyValue) {
 //     if (output.value == ".") {
-//         output.value = "";
-//         output.value = output.value + anyValue; 
-        
+    //         output.value = "";
+    //         output.value = output.value + anyValue; 
+    
 //     } else {
 //         output.value=eval(output.value)
 //     }
 // }
 
 
+// let array = [];
+// output.value = output.value + anyNumber;
+// array.push(output.value)
+// if (array.includes(".")) {
+//     output.value = "";
+//     } else {
+//     output.value=eval(output.value)
+// }
